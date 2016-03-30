048e6928d7f0ba548f39bcdb9e6537ee3b8605a8
Steve McCarthy <steve@redlua.com>
2016-03-30 15:56:45 +0900

    Add session name context var

--- main.go
+++ main.go
@@ -27,12 +27,15 @@ func setup() (*Context, string, error) {
 		defaultSessionSecret = "session-secret"
 		defaultDbURL         = "file:data/sqlite.db"
 		defaultPort          = 3000
+		defaultSessionName   = "session-name"
 		sessionSecret        = flag.String("session", defaultSessionSecret, "Set the session secret")
 		dbURL                = flag.String("db", defaultDbURL, "Set the database connection string")
 		port                 = flag.Int("port", defaultPort, "Set the server port")
+		sessionName          = flag.String("session-name", defaultSessionName, "Set the session name")
 		envSessionSecret     = os.Getenv("SESSION_SECRET")
 		envDbURL             = os.Getenv("DATABASE_URL")
 		outPort              = os.Getenv("PORT")
+		envSessionName       = os.Getenv("SESSION_NAME")
 	)
 	flag.Parse()
 
@@ -45,6 +48,9 @@ func setup() (*Context, string, error) {
 	if *port != defaultPort || outPort == "" {
 		outPort = strconv.Itoa(defaultPort)
 	}
+	if *sessionName == defaultSessionName && envSessionName != "" {
+		sessionName = &envSessionName
+	}
 
 	store := sessions.NewCookieStore([]byte(*sessionSecret))
 	db, err := SetupDB(*dbURL)
@@ -55,9 +61,10 @@ func setup() (*Context, string, error) {
 }
 
 type Context struct {
-	Vars  map[string]string
-	DB    *sql.DB
-	Store *sessions.CookieStore
+	Vars        map[string]string
+	DB          *sql.DB
+	Store       *sessions.CookieStore
+	SessionName string
 }
 
 func router(c Context) *mux.Router {
