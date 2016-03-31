e43053e70cea59ad97231722c707d71bf5ca6c7c
Steve McCarthy <steve@redlua.com>
2016-04-01 07:18:08 +0900

    Add middleware, use middleware to inject context

--- route.go
+++ route.go
@@ -4,25 +4,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	// TODO use sessions
-	//"github.com/gorilla/sessions"
 )
 
 func router(c Context) *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/", injectContext(indexHandler, c)).Methods("GET")
-	r.HandleFunc("/login", injectContext(loginHandler, c)).Methods("POST")
+	r.HandleFunc("/", addMiddleware(indexHandler, c)).Methods("GET")
+	r.HandleFunc("/login", addMiddleware(loginHandler, c)).Methods("POST")
 	return r
 }
 
 type contextHandler func(http.ResponseWriter, *http.Request, Context)
+type middleware func(contextHandler) contextHandler
 
-func injectContext(fn contextHandler, c Context) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		c.Vars = mux.Vars(r)
-		fn(w, r, c)
-	}
-}
 
 func indexHandler(w http.ResponseWriter, r *http.Request, c Context) {
 	w.Write([]byte("Under construction"))
@@ -30,4 +23,14 @@ func indexHandler(w http.ResponseWriter, r *http.Request, c Context) {
 
 func loginHandler(w http.ResponseWriter, r *http.Request, c Context) {
 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
+
+func addMiddleware(ch contextHandler, c Context, ms ...middleware) http.HandlerFunc {
+	for i := len(ms) - 1; i >= 0; i-- {
+		ch = ms[i](ch)
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		c.Vars = mux.Vars(r)
+		ch(w, r, c)
+	}
 }
