acc653ec844595916ec89837166d7b0a55add0c8
Steve McCarthy <steve@redlua.com>
2016-04-03 17:07:32 +0900

    Add PreparedStatments setup

--- database.go
+++ database.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -24,4 +25,35 @@ func SetupDB(c DBConfig) (*sql.DB, error) {
 	// TODO setup the tables
 
 	return db, nil
+}
+
+type PreparedStatements struct {
+	Login   *sql.Stmt
+	NewUser *sql.Stmt
+}
+
+func SetupStmts(db *sql.DB, c DBConfig) (*PreparedStatements, error) {
+	var err error
+	stmts := &PreparedStatements{}
+	stmts.Login, err = CreateLoginQuery(db, c)
+	if err != nil {
+		return nil, err
+	}
+	stmts.NewUser, err = CreateNewUserQuery(db, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return stmts, nil
+}
+
+func CreateLoginQuery(db *sql.DB, c DBConfig) (*sql.Stmt, error) {
+	stmt := fmt.Sprintf("SELECT password FROM %s WHERE email = $1;", c.UserTableName)
+	return db.Prepare(stmt)
+}
+
+func CreateNewUserQuery(db *sql.DB, c DBConfig) (*sql.Stmt, error) {
+	stmt := fmt.Sprintf("INSERT INTO %s (email, username, password) VALUES ($1, $2, $3);",
+		c.UserTableName)
+	return db.Prepare(stmt)
 }
