68b1fe4d08653ffcfad0dd769aa12d83d4780e56
Steve McCarthy <steve@redlua.com>
2016-04-06 07:06:00 +0900

    Add user creation route

--- route.go
+++ route.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func router(c Context) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", addMiddleware(indexHandler, c, isAuthed)).Methods("GET")
 	r.HandleFunc("/login", addMiddleware(loginHandler, c)).Methods("GET", "POST")
+	r.HandleFunc("/newuser", addMiddleware(newUserHandler, c)).Methods("POST")
 	return r
 }
 
@@ -22,6 +25,63 @@ func indexHandler(w http.ResponseWriter, r *http.Request, c Context) {
 
 func loginHandler(w http.ResponseWriter, r *http.Request, c Context) {
 	w.Write([]byte("Login under construction"))
+}
+
+func newUserHandler(w http.ResponseWriter, r *http.Request, c Context) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	passwordCheck := r.FormValue("repeat")
+
+	// TODO for all errors redirect and show flash message
+	if email == "" {
+		password, passwordCheck = "", ""
+		http.Error(w, "email not provided", http.StatusBadRequest)
+		return
+	}
+
+	if password != passwordCheck {
+		password, passwordCheck = "", ""
+		http.Error(w, "passwords do not match", http.StatusBadRequest)
+		return
+	}
+
+	// TODO handle database connection error
+	if _, err := c.PS.GetUser.Query(email); err != nil && err != sql.ErrNoRows {
+		password, passwordCheck = "", ""
+		http.Error(w, "email is already taken", http.StatusBadRequest)
+		return
+	}
+
+	// save user
+	bcryptPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	password, passwordCheck = "", ""
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	newUserQuery := c.PS.NewUser.QueryRow(email, bcryptPass)
+	zero(bcryptPass)
+	var userID int64
+	err = newUserQuery.Scan(&userID)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	// login
+	session, err := c.Store.Get(r, c.SessionName)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	user := &User{ID: userID, Email: email}
+	session.Values["user"] = user
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	http.Redirect(w, r, "/", 303)
 }
 
 func addMiddleware(ch contextHandler, c Context, ms ...middleware) http.HandlerFunc {
@@ -50,4 +110,10 @@ func isAuthed(ch contextHandler) contextHandler {
 		ch(w, r, c)
 	}
 	return outFunc
+}
+
+func zero(bts []byte) {
+	for i := range bts {
+		bts[i] = 0
+	}
 }
